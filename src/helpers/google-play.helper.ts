14f6275ec6421c004d4ddb034f76fe4c132654db
SBats <simon.bats12@gmail.com>
2017-08-22 16:53:33 -0400

    Improve google play helper

--- src/helpers/google-play.helper.ts
+++ src/helpers/google-play.helper.ts
@@ -1,14 +1,15 @@
 import { GooglePlayConstants } from '../constants/google-play'
 
 export class GooglePlayHelper {
-  public static getAppInfos() {
-    return GooglePlayHelper._getAppPage()
+  public static getAppInfos(bundleID) {
+    return GooglePlayHelper._getAppPage(bundleID)
+      .then(GooglePlayHelper._handleNotFound)
       .then(GooglePlayHelper._convertHTMLToText)
       .then(GooglePlayHelper._parseResource)
   }
 
-  private static _getAppPage() {
-    return fetch(`${GooglePlayConstants.PLAY_STORE_ROOT_WEB}com.shots.android`)
+  private static _getAppPage(bundleID) {
+    return fetch(`${GooglePlayConstants.PLAY_STORE_ROOT_WEB}${bundleID}`)
   }
 
 
@@ -18,7 +19,14 @@ export class GooglePlayHelper {
     return { version, date }
   }
 
-  private static _convertHTMLToText(resource) {
-    return resource.text()
+  private static _handleNotFound(response) {
+    if (response.status === 404) {
+      throw new Error(GooglePlayConstants.PLAY_STORE_PACKAGE_NOT_PUBLISHED_IDENTIFIER)
+    }
+    return response
+  }
+
+  private static _convertHTMLToText(response) {
+    return response.text()
   }
 }
