ccdde8148a7574127ed2aaa281ac71278f17cf78
SBats <simon.bats12@gmail.com>
2017-08-29 17:43:33 -0400

    Remove generated d.ts file from versioning

--- src/constants/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./alert-types";
-export * from "./app-store";
-export * from "./countries";
-export * from "./google-play";
-export * from "./locales";
-export * from "./update-types";
--- src/interfaces/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './app-store-result.interface';
-export * from './google-play-result.interface';
-export * from './store-update.config.interface';
