0ef44384f7784338f8a2b93eab9b24c267469dc9
SBats <simon.bats12@gmail.com>
2017-10-12 10:23:18 -0400

    Remove reference to i18n in references.d.ts as it's a peer dependency

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="./node_modules/nativescript-i18n/references.d.ts" />
-
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
